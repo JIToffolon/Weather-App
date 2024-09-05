2cef345160042740d505b1e44a05d2f622a6c382
Juan Ignacio Toffolon <juanignaciotoffolon@gmail.com>
2024-09-05 16:20:52 -0300

    Creación de app

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Box, Container, Typography } from "@mui/material";
+
+
+
+function App() {
+
+  return (
+    <>
+     <Container>
+      <Box>
+        <Typography color="indigo" variant="h5">
+            Weather App
+        </Typography>
+      </Box>
+     </Container>
+    </>
+  )
+}
+
+export default App
